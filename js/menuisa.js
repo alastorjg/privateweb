fa6bf142bc3e898d9a8ef7b88eb4347f37b62afe
julio <jj-gr.23@hotmail.com>
2019-10-07 00:54:19 -0500

    varios

--- js/menuisa.js
+++ js/menuisa.js
@@ -1,71 +1,71 @@
-(function($) {
+// (function($) {
 
-    $.fn.menumaker = function(options) {
+//     $.fn.menumaker = function(options) {
 
-        var cssmenu = $(this),
-            settings = $.extend({
-                title: "Menu",
-                format: "dropdown",
-                sticky: false
-            }, options);
+//         var cssmenu = $(this),
+//             settings = $.extend({
+//                 title: "Menu",
+//                 format: "dropdown",
+//                 sticky: false
+//             }, options);
 
-        return this.each(function() {
-            cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
-            $(this).find("#menu-button").on('click', function() {
-                $(this).toggleClass('menu-opened');
-                var mainmenu = $(this).next('ul');
-                if (mainmenu.hasClass('open')) {
-                    mainmenu.hide().removeClass('open');
-                } else {
-                    mainmenu.show().addClass('open');
-                    if (settings.format === "dropdown") {
-                        mainmenu.find('ul').show();
-                    }
-                }
-            });
+//         return this.each(function() {
+//             cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+//             $(this).find("#menu-button").on('click', function() {
+//                 $(this).toggleClass('menu-opened');
+//                 var mainmenu = $(this).next('ul');
+//                 if (mainmenu.hasClass('open')) {
+//                     mainmenu.hide().removeClass('open');
+//                 } else {
+//                     mainmenu.show().addClass('open');
+//                     if (settings.format === "dropdown") {
+//                         mainmenu.find('ul').show();
+//                     }
+//                 }
+//             });
 
-            cssmenu.find('li ul').parent().addClass('has-sub');
+//             cssmenu.find('li ul').parent().addClass('has-sub');
 
-            multiTg = function() {
-                cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
-                cssmenu.find('.submenu-button').on('click', function() {
-                    $(this).toggleClass('submenu-opened');
-                    if ($(this).siblings('ul').hasClass('open')) {
-                        $(this).siblings('ul').removeClass('open').hide();
-                    } else {
-                        $(this).siblings('ul').addClass('open').show();
-                    }
-                });
-            };
+//             multiTg = function() {
+//                 cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+//                 cssmenu.find('.submenu-button').on('click', function() {
+//                     $(this).toggleClass('submenu-opened');
+//                     if ($(this).siblings('ul').hasClass('open')) {
+//                         $(this).siblings('ul').removeClass('open').hide();
+//                     } else {
+//                         $(this).siblings('ul').addClass('open').show();
+//                     }
+//                 });
+//             };
 
-            if (settings.format === 'multitoggle') multiTg();
-            else cssmenu.addClass('dropdown');
+//             if (settings.format === 'multitoggle') multiTg();
+//             else cssmenu.addClass('dropdown');
 
-            if (settings.sticky === true) cssmenu.css('position', 'fixed');
+//             if (settings.sticky === true) cssmenu.css('position', 'fixed');
 
-            resizeFix = function() {
-                if ($(window).width() > 768) {
-                    cssmenu.find('ul').show();
-                }
+//             resizeFix = function() {
+//                 if ($(window).width() > 768) {
+//                     cssmenu.find('ul').show();
+//                 }
 
-                if ($(window).width() <= 768) {
-                    cssmenu.find('ul').hide().removeClass('open');
-                }
-            };
-            resizeFix();
-            return $(window).on('resize', resizeFix);
+//                 if ($(window).width() <= 768) {
+//                     cssmenu.find('ul').hide().removeClass('open');
+//                 }
+//             };
+//             resizeFix();
+//             return $(window).on('resize', resizeFix);
 
-        });
-    };
-})(jQuery);
+//         });
+//     };
+// })(jQuery);
 
-(function($) {
-    $(document).ready(function() {
+// (function($) {
+//     $(document).ready(function() {
 
-        $("#cssmenu").menumaker({
-            title: "Menu",
-            format: "multitoggle"
-        });
+//         $("#cssmenu").menumaker({
+//             title: "Menu",
+//             format: "multitoggle"
+//         });
 
-    });
-})(jQuery);
+//     });
+// })(jQuery);
